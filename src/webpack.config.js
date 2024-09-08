17a2bde95056b3a40e47e7725950fef7cfbc8314
Sophia G <97923995+User1245677977@users.noreply.github.com>
2024-09-07 22:00:15 -0400

    Fix Webpack 5 polyfills

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -17,3 +17,25 @@ module.exports = {
     })
   ]
 };
+const webpack = require('webpack');
+
+module.exports = {
+  resolve: {
+    fallback: {
+      "fs": false, // fs cannot be polyfilled for the browser
+      "path": require.resolve("path-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "crypto": require.resolve("crypto-browserify"),
+      "zlib": require.resolve("browserify-zlib"),
+      "querystring": require.resolve("querystring-es3"),
+      "url": require.resolve("url/"),
+      "util": require.resolve("util/")
+    }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
+};
