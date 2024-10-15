ffdf69880d520a40662ff61dc37d7b83e3d39e8d
Sophia G <97923995+User1245677977@users.noreply.github.com>
2024-10-15 15:31:03 -0400

    Fixed Sequelize config and database model

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,17 +4,17 @@ module.exports = {
     password: process.env.DB_PASSWORD || "pt91iuay7r6lz61m",
     database: process.env.DB_NAME || "k074fnbxna9d128p",
     host: process.env.DB_HOST || "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    dialect: "mysql",  // Use MySQL dialect
+    dialect: "mysql",
   },
   test: {
     username: process.env.DB_USERNAME || "ka0a54erd9z2becm",
     password: process.env.DB_PASSWORD || "pt91iuay7r6lz61m",
     database: process.env.DB_NAME || "k074fnbxna9d128p",
     host: process.env.DB_HOST || "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    dialect: "mysql",  // Use MySQL dialect
+    dialect: "mysql",
   },
   production: {
-    use_env_variable: 'JAWSDB_URL', // Use JAWSDB_URL environment variable in production
+    use_env_variable: 'JAWSDB_URL',
     dialect: 'mysql',
     dialectOptions: {
       ssl: {
