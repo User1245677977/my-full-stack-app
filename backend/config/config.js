9ff3aeb99373e1c55032bb3eface76f676735e41
Sophia G <97923995+User1245677977@users.noreply.github.com>
2024-10-16 14:02:59 -0400

    fixed code

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,30 +1,28 @@
 //config.js
 
-console.log('JAWSDB_URL:', process.env.JAWSDB_URL);
-
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || "ka0a54erd9z2becm",
-    password: process.env.DB_PASSWORD || "pt91iuay7r6lz61m",
-    database: process.env.DB_NAME || "k074fnbxna9d128p",
-    host: process.env.DB_HOST || "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    dialect: "mysql",
+    username: process.env.DB_USERNAME || 'ka0a54erd9z2becm',
+    password: process.env.DB_PASSWORD || 'pt91iuay7r6lz61m',
+    database: process.env.DB_NAME || 'k074fnbxna9d128p',
+    host: process.env.DB_HOST || 'p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    dialect: 'mysql',
   },
   test: {
-    username: process.env.DB_USERNAME || "ka0a54erd9z2becm",
-    password: process.env.DB_PASSWORD || "pt91iuay7r6lz61m",
-    database: process.env.DB_NAME || "k074fnbxna9d128p",
-    host: process.env.DB_HOST || "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    dialect: "mysql",
+    username: process.env.DB_USERNAME || 'ka0a54erd9z2becm',
+    password: process.env.DB_PASSWORD || 'pt91iuay7r6lz61m',
+    database: process.env.DB_NAME || 'k074fnbxna9d128p',
+    host: process.env.DB_HOST || 'p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    dialect: 'mysql',
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'DATABASE_URL',  // Ensure you're using 'DATABASE_URL'
     dialect: 'mysql',
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false,
+        rejectUnauthorized: false,  // Disable SSL verification for connection
       },
     },
-  },  
+  },
 };
