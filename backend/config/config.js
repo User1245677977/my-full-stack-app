fd7b76ccd561aba6469bba9a10c3755d93054c7b
Sophia G <97923995+User1245677977@users.noreply.github.com>
2024-10-17 15:24:36 -0400

    adjust to postgres

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,21 +1,23 @@
+//config.js
+
 module.exports = {
   development: {
     username: process.env.DB_USERNAME || 'u8m4lr3njuuh62',
     password: process.env.DB_PASSWORD || 'pd95086458e6ba1f98ef1c7c4599bffd7a0bc32ae4fdcc0960f8f999833498162',
     database: process.env.DB_NAME || 'ddgfq2lnkkpmgj',
     host: process.env.DB_HOST || 'c3nv2ev86aje4j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
-    dialect: 'postgres',  // Change from 'mysql' to 'postgres'
+    dialect: 'postgres', 
   },
   test: {
-    username: process.env.DB_USERNAME || 'your_postgres_username',
-    password: process.env.DB_PASSWORD || 'your_postgres_password',
-    database: process.env.DB_NAME || 'your_postgres_database',
-    host: process.env.DB_HOST || 'your_postgres_host',
-    dialect: 'postgres',  // Change from 'mysql' to 'postgres'
+    username: process.env.DB_USERNAME || 'u8m4lr3njuuh62',
+    password: process.env.DB_PASSWORD || 'pd95086458e6ba1f98ef1c7c4599bffd7a0bc32ae4fdcc0960f8f999833498162',
+    database: process.env.DB_NAME || 'ddgfq2lnkkpmgj',
+    host: process.env.DB_HOST || 'c3nv2ev86aje4j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+    dialect: 'postgres', 
   },
   production: {
     use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',  // Change from 'mysql' to 'postgres'
+    dialect: 'postgres', 
     dialectOptions: {
       ssl: {
         require: true,
