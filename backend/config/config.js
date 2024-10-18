acf72406a98fb9591b3e5de25276cf018f5c4028
Sophia G <97923995+User1245677977@users.noreply.github.com>
2024-10-18 16:15:44 -0400

    adjust config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,24 +6,25 @@ module.exports = {
     password: process.env.DB_PASSWORD || 'pd95086458e6ba1f98ef1c7c4599bffd7a0bc32ae4fdcc0960f8f999833498162',
     database: process.env.DB_NAME || 'ddgfq2lnkkpmgj',
     host: process.env.DB_HOST || 'c3nv2ev86aje4j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
-    dialect: 'postgres', // Changed to 'postgres'
+    dialect: 'postgres',
   },
   test: {
     username: process.env.DB_USERNAME || 'u8m4lr3njuuh62',
     password: process.env.DB_PASSWORD || 'pd95086458e6ba1f98ef1c7c4599bffd7a0bc32ae4fdcc0960f8f999833498162',
     database: process.env.DB_NAME || 'ddgfq2lnkkpmgj',
     host: process.env.DB_HOST || 'c3nv2ev86aje4j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
-    dialect: 'postgres', // Changed to 'postgres'
+    dialect: 'postgres',
   },
   production: {
     use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres', // Ensure it's 'postgres'
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false,  // Ensure SSL is handled correctly
+        rejectUnauthorized: false,
       },
     },
   },
 };
+
 
