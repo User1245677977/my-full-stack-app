2f35e9dc83dffc0842ae2ae6a3ba0680b4fbadf7
Sophia G <97923995+User1245677977@users.noreply.github.com>
2024-10-22 13:31:38 -0400

    Remove lingering MySQL references and update for PostgreSQL

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,17 +2,17 @@
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || 'u8m4lr3njuuh62',
-    password: process.env.DB_PASSWORD || 'pd95086458e6ba1f98ef1c7c4599bffd7a0bc32ae4fdcc0960f8f999833498162',
-    database: process.env.DB_NAME || 'ddgfq2lnkkpmgj',
-    host: process.env.DB_HOST || 'c3nv2ev86aje4j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+    username: process.env.DB_USERNAME || 'your_postgres_username',
+    password: process.env.DB_PASSWORD || 'your_postgres_password',
+    database: process.env.DB_NAME || 'your_postgres_db_name',
+    host: process.env.DB_HOST || 'your_postgres_host',
     dialect: 'postgres',
   },
   test: {
-    username: process.env.DB_USERNAME || 'u8m4lr3njuuh62',
-    password: process.env.DB_PASSWORD || 'pd95086458e6ba1f98ef1c7c4599bffd7a0bc32ae4fdcc0960f8f999833498162',
-    database: process.env.DB_NAME || 'ddgfq2lnkkpmgj',
-    host: process.env.DB_HOST || 'c3nv2ev86aje4j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+    username: process.env.DB_USERNAME || 'your_postgres_username',
+    password: process.env.DB_PASSWORD || 'your_postgres_password',
+    database: process.env.DB_NAME || 'your_postgres_db_name',
+    host: process.env.DB_HOST || 'your_postgres_host',
     dialect: 'postgres',
   },
   production: {
@@ -24,7 +24,5 @@ module.exports = {
         rejectUnauthorized: false,
       },
     },
-  }  
+  },
 };
-
-
