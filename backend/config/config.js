f1b1aa68849a35cce6fc4a1b156f1144eb56f582
Sophia G <97923995+User1245677977@users.noreply.github.com>
2024-10-22 13:14:25 -0400

    Switched to PostgreSQL and removed MySQL references

--- backend/config/config.js
+++ backend/config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    use_env_variable: 'DATABASE_URL', // This should match your Heroku config
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
@@ -24,7 +24,7 @@ module.exports = {
         rejectUnauthorized: false,
       },
     },
-  },
+  }  
 };
 
 
